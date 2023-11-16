e8f83027917d820201dbd2b87a7983df82844d22
Marcus Holje <99866281+norskemarcus@users.noreply.github.com>
2023-11-16 08:50:55 +0100

    new branch feature to change auth system to bcrypt

--- client/src/pages/Home/Home.svelte
+++ client/src/pages/Home/Home.svelte
@@ -12,7 +12,14 @@
   <div class="flex-grow-1 ms-3">
     <h3 class="mt-0">Are you ready to learn some svelte?</h3>
 
-    <p>This is my little tutorial for understanding svelte and maybe you can learn some as well? Play around</p>
+    <p>
+      This is my little tutorial for understanding svelte and maybe you can learn some as well? Play around. First of all, try out my dark mode by swiping the slider in the upper right corner. This I have made with sveltestrap. The responsive navbar is a wellknown bootstrap design, also to be found
+      in sveltestrap.
+    </p>
+
+    <p>You have to signup or login to get the best user experience. The best stuff is here!</p>
+
+    <p>This page is developed by computer science student at KEA, Marcus Holje</p>
   </div>
   <div class="flex-shrink-0">
     <img src="#" alt="" width="" />
